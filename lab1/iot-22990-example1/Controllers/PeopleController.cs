fb813dc6cd51ed7a476b9849433b106b6b296a93
ciszakdamian <ciszakdamian@gmail.com>
2020-10-16 23:36:03 +0200

    added people controller

---
+++ lab1/iot-22990-example1/Controllers/PeopleController.cs
@@ -0,0 +1,43 @@
+﻿using iot_22990_example1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iot_22990_example1.Controllers
+{
+    [ApiController]
+    [Route("api/people")]
+
+    public class PeopleController : ControllerBase
+    {
+        private List<Person> people;
+
+        public PeopleController()
+        {
+            people = new List<Person>()
+            {
+                new Person
+                {
+                FirstName = "Damian",
+                LastName = "Ciszak",
+                Id = 1
+                },
+                new Person
+                {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                Id = 2
+                }
+            };
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(people);
+        }
+    }
+}
---
+++ lab1/iot-22990-example1/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iot_22990_example1.Models
+{
+    public class Person
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
